9f9a8553a669e4e77cdff233671b9f8527cc4827
Jaideep <45902000+JaideepYadav910@users.noreply.github.com>
2019-01-27 13:13:59 +0530

    source_code_here

---
+++ paragraph writing program.py
@@ -0,0 +1,11 @@
+# Paragraph writing program
+# using while loop
+
+print ("Below is the open space for you to write the paragraph!\n\n\n")
+
+string = " "
+
+while "The End" != string:
+    string = input ()
+
+input("Press Enter key to exit.")
